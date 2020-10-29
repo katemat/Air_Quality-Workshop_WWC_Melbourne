e9fa2f5556443ea6850dd5179e0babb4d85a04f1
Katerina <matveevakaterinka@gmail.com>
2020-10-29 17:24:32 +1100

    Initial Commit

---
+++ AirQualityWorkShop/AirQualityWorkShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirQualityWorkShopApp.swift
+//  AirQualityWorkShop
+//
+//  Created by Katerina on 29/10/20.
+//
+
+import SwiftUI
+
+@main
+struct AirQualityWorkShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirQualityWorkShop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AirQualityWorkShop
+//
+//  Created by Katerina on 29/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
